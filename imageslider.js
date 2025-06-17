abf5f22f8d0dbc5dc831af90546edb18b9dfa9c4
Priyanka Gupta <101priyag@gmail.com>
2025-06-17 12:56:21 +0530

    2nd commit

---
+++ imageslider.js
@@ -0,0 +1,25 @@
+export const imageslider =[
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/61311f765f6a51f6.png?q=20"
+},
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/6808ca3abb3da0eb.jpg?q=20"
+},
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/8948611244f67dc6.jpeg?q=20"
+},
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/34fcbf942d20fe6b.jpg?q=20"
+},
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/2d46d868f6692aeb.jpg?q=20"
+},
+
+{
+    img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/48d3b0ee90e0e632.jpeg?q=20"
+},
+]
+
+
+
+
